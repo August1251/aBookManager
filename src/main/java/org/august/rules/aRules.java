26dcb6727c6f8e51ae04dc2cc3a1aedf9f027689
August1251 <empty>
2025-01-26 13:50:08 +0300

    feat. The project created.

---
+++ src/main/java/org/august/rules/aRules.java
@@ -0,0 +1,17 @@
+package org.august.rules;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class aRules extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
