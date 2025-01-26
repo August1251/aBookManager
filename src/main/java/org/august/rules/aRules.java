7b0c9b2931d85f536208cdd66cccef723604fa61
August1251 <empty>
2025-01-26 14:55:44 +0300

    feat. Updated spigot version, added config.yml, rules configuration and book dto. Updated main class.

--- src/main/java/org/august/rules/aRules.java
+++ src/main/java/org/august/rules/aRules.java
@@ -1,17 +1,24 @@
 package org.august.rules;
 
+import org.august.rules.command.RulesCommand;
+import org.august.rules.config.RulesConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class aRules extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        saveDefaultConfig();
+        configure();
+        registerCommands();
     }
 
-    @Override
-    public void onDisable() {
+    private void configure() {
+        RulesConfiguration.setRules(this);
+    }
 
+    private void registerCommands() {
+        getCommand("rules").setExecutor(new RulesCommand(this));
     }
 
 }
